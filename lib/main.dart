da6ac1af275b8a4de4fccf50982e3b88b3550792
Alex Felipe <alexfelipevieira@gmail.com>
2019-12-09 09:03:14 -0300

    formulário simples

---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final TextEditingController _controladorNome = TextEditingController();
+  final TextEditingController _controladorQuantidade = TextEditingController();
+  final TextEditingController _controladorValor = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Cadastrando produto'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: _controladorNome,
+                decoration: InputDecoration(labelText: 'Nome'),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: TextField(
+                  controller: _controladorQuantidade,
+                  decoration: InputDecoration(labelText: 'Quantidade'),
+                  keyboardType: TextInputType.number,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: TextField(
+                  controller: _controladorValor,
+                  decoration: InputDecoration(labelText: 'Valor'),
+                  keyboardType: TextInputType.number,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: RaisedButton(
+                  child: Text('Cadastrar'),
+                  onPressed: () {
+                    final String nome = _controladorNome.text;
+                    final int quantidade =
+                        int.tryParse(_controladorQuantidade.text);
+                    final double valor =
+                        double.tryParse(_controladorValor.text);
+
+                    final Produto produtoNovo =
+                        Produto(nome, quantidade, valor);
+                    print(produtoNovo);
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Produto {
+  final String nome;
+  final int quantidade;
+  final double valor;
+
+  Produto(
+    this.nome,
+    this.quantidade,
+    this.valor,
+  );
+
+  @override
+  String toString() {
+    return 'Produto{nome: $nome, quantidade: $quantidade, valor: $valor}';
+  }
+}
